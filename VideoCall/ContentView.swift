58cad1ca3ee42842e97220bc7181e6b77ef933e2
rockgard3n <liamcashel@gmail.com>
2024-02-07 15:52:22 -0800

    Initial Commit

---
+++ VideoCall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VideoCall
+//
+//  Created by Liam Cashel on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VideoCall/VideoCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCallApp.swift
+//  VideoCall
+//
+//  Created by Liam Cashel on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
