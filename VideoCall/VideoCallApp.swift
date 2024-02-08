db82c8d4fdd66c2470c3f37c196d765c6137f3b2
rockgard3n <liamcashel@gmail.com>
2024-02-07 16:46:19 -0800

    testing out stream capabilities

--- VideoCall/VideoCallApp.swift
+++ VideoCall/VideoCallApp.swift
@@ -6,12 +6,146 @@
 //
 
 import SwiftUI
+import StreamVideo
+import StreamVideoSwiftUI
 
 @main
 struct VideoCallApp: App {
+    @State var call: Call
+    @ObservedObject var state: CallState
+    @State var callCreated: Bool = false
+
+    //for a demo go to this link: https://getstream.io/video/sdk/ios/tutorial/video-calling/ then step 3 and there is sample api keys and tokens and stuff
+    
+    private var client: StreamVideo
+    private let apiKey: String = "mmhfdzb5evj2" // The API key can be found in the Credentials section
+    private let userId: String = "Cad_Bane" // The User Id can be found in the Credentials section
+    private let token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiQ2FkX0JhbmUiLCJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0NhZF9CYW5lIiwiaWF0IjoxNzA3MzUxNjc1LCJleHAiOjE3MDc5NTY0ODB9.qmdU-x4y_fOdJlW6_Or9Gk-wc2oCdoIA_JFi3A_YoHE" // The Token can be found in the Credentials section
+    private let callId: String = "5v2Gw9BBZrMs" // The CallId can be found in the Credentials section
+
+    init() {
+        let user = User(
+            id: userId,
+            name: "BOB SAGET", // name and imageURL are used in the UI
+            imageURL: .init(string: "https://getstream.io/static/2796a305dd07651fcceb4721a94f4505/a3911/martin-mitrevski.webp")
+        )
+
+        // Initialize Stream Video client
+        self.client = StreamVideo(
+            apiKey: apiKey,
+            user: user,
+            token: .init(stringLiteral: token)
+        )
+
+        // Initialize the call object
+        let call = client.call(callType: "default", callId: callId)
+
+        self.call = call
+        self.state = call.state
+    }
+    
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            VStack {
+                if callCreated {
+                    ZStack {
+                        ParticipantsView(
+                            call: call,
+                            participants: call.state.remoteParticipants,
+                            onChangeTrackVisibility: changeTrackVisibility(_:isVisible:)
+                        )
+                        FloatingParticipantView(participant: call.state.localParticipant)
+                    }
+                } else {
+                    Text("loading...")
+                }
+            }.onAppear {
+                Task {
+                    guard callCreated == false else { return }
+                    try await call.join(create: true)
+                    callCreated = true
+                }
+            }
+        }
+    }
+
+    /// Changes the track visibility for a participant (not visible if they go off-screen).
+    /// - Parameters:
+    ///  - participant: the participant whose track visibility would be changed.
+    ///  - isVisible: whether the track should be visible.
+    private func changeTrackVisibility(_ participant: CallParticipant?, isVisible: Bool) {
+        guard let participant else { return }
+        Task {
+            await call.changeTrackVisibility(for: participant, isVisible: isVisible)
         }
     }
 }
+
+struct ParticipantsView: View {
+
+    var call: Call
+    var participants: [CallParticipant]
+    var onChangeTrackVisibility: (CallParticipant?, Bool) -> Void
+
+    var body: some View {
+        GeometryReader { proxy in
+            if !participants.isEmpty {
+                ScrollView {
+                    LazyVStack {
+                        if participants.count == 1, let participant = participants.first {
+                            makeCallParticipantView(participant, frame: proxy.frame(in: .global))
+                                .frame(width: proxy.size.width, height: proxy.size.height)
+                        } else {
+                            ForEach(participants) { participant in
+                                makeCallParticipantView(participant, frame: proxy.frame(in: .global))
+                                    .frame(width: proxy.size.width, height: proxy.size.height / 2)
+                            }
+                        }
+                    }
+                }
+            } else {
+                Color.black
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+
+    @ViewBuilder
+    private func makeCallParticipantView(_ participant: CallParticipant, frame: CGRect) -> some View {
+        VideoCallParticipantView(
+            participant: participant,
+            availableFrame: frame,
+            contentMode: .scaleAspectFit,
+            customData: [:],
+            call: call
+        )
+        .onAppear { onChangeTrackVisibility(participant, true) }
+        .onDisappear{ onChangeTrackVisibility(participant, false) }
+    }
+}
+
+struct FloatingParticipantView: View {
+
+    var participant: CallParticipant?
+    var size: CGSize = .init(width: 120, height: 120)
+
+    var body: some View {
+        if let participant = participant {
+            VStack {
+                HStack {
+                    Spacer()
+
+                    VideoRendererView(id: participant.id, size: size) { videoRenderer in
+                        videoRenderer.handleViewRendering(for: participant, onTrackSizeUpdate: { _, _ in })
+                    }
+                    .frame(width: size.width, height: size.height)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
